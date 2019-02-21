6927e22142459dcfea0588b7da76342e9ddeef4b
sculptormuur <amorey@yandex.ru>
2019-02-21 00:22:00 +0000

    4.2 предохранение от перезаписи, надо переделать ограничение типов

--- homework-php1_2019-01-31_02/upload.php
+++ homework-php1_2019-01-31_02/upload.php
@@ -22,24 +22,39 @@
 
 <?php
 // const IMG_ADDRESS_PART = __DIR__ . '/images/' . 'img';
+const UPLOAD_ERROR = 'Ошибка! Файл не загружен.';
 
 if (isset($_FILES['newFile']) && (0 == $_FILES['newFile']['error'])) { // некрасиво
     var_dump($_FILES['newFile']);
     if (1 == (preg_match('/\.(jpe??g|png|gif|bmp|tif{1,2})$/i',
             $_FILES['newFile']['name'], $extensionArr))) {
         var_dump($extensionArr);
-//        $newImgAddress = __DIR__ . '/images/' . uniqid() . $extensionArr[0];
+//       $newImgAddress = __DIR__ . '/images/' . uniqid() . $extensionArr[0];
         $newImgAddress = __DIR__ . '/images/' . '5c6ddc7f77678' . $extensionArr[0];
         echo $newImgAddress, ' ';
-        $uploadedFile = move_uploaded_file($_FILES['newFile']['tmp_name'], $newImgAddress);
 
-        if (file_exists($newImgAddress)) { // если с таким именем уже был файл7
-            echo 'Файл загружен.';
+        /* Преддохранение от перезаписи в случае совпадения имени файла. */
+        $i = 0;
+        while (true == (file_exists($newImgAddress))) { // бесконечность
+            $newImgAddress = __DIR__ . '/images/' . '5c6ddc7f77678' . $extensionArr[0];
+            $i++;
+            echo ' цикл ', $i;
+            assert(100 >= $i);
+            if (100 <= $i) {
+                $signal = 1;
+                break;
+            }
+        }
+        if (1 != $signal) {
+            $uploadedFile = move_uploaded_file($_FILES['newFile']['tmp_name'], $newImgAddress);
+            if (file_exists($newImgAddress)) { // если с таким именем уже был файл7
+                echo 'Файл загружен.';
+            }
         } else {
-            echo 'Ошибка! Файл не загружен.';
+            echo UPLOAD_ERROR;
         }
     } else {
-        echo 'Ошибка! Файл не загружен.';
+        echo UPLOAD_ERROR;
     }
     unset($_FILES['newFile']);
 }
