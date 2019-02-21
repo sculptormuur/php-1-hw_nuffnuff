d42e674892326857e8d14d8eec138dc6dbcebed2
sculptormuur <amorey@yandex.ru>
2019-02-21 02:38:08 +0000

    4.2 100500 видов проверки типа файла, грязь грязь!

--- homework-php1_2019-01-31_02/upload.php
+++ homework-php1_2019-01-31_02/upload.php
@@ -16,45 +16,55 @@
 ?>
 
 <form action="upload.php" method="post" id="imgFile" enctype="multipart/form-data">
-    <input type="file" name="newFile" size="70" value="" form="imgFile" required/>
+    <input type="file" name="newFile" size="70" value="" form="imgFile" accept="image/*" required/>
     <input name="submitFile" type="submit" value="Отправить" form="imgFile">
 </form>
 
 <?php
-// const IMG_ADDRESS_PART = __DIR__ . '/images/' . 'img';
 const UPLOAD_ERROR = 'Ошибка! Файл не загружен.';
 
 if (isset($_FILES['newFile']) && (0 == $_FILES['newFile']['error'])) { // некрасиво
     var_dump($_FILES['newFile']);
-    if (1 == (preg_match('/\.(jpe??g|png|gif|bmp|tif{1,2})$/i',
-            $_FILES['newFile']['name'], $extensionArr))) {
-        var_dump($extensionArr);
-//       $newImgAddress = __DIR__ . '/images/' . uniqid() . $extensionArr[0];
-        $newImgAddress = __DIR__ . '/images/' . '5c6ddc7f77678' . $extensionArr[0];
-        echo $newImgAddress, ' ';
+    if (false !== (stripos($_FILES['newFile']['type'], 'image'))) {
+        $dudu = exif_imagetype($_FILES['newFile']['tmp_name']);
+        var_dump(image_type_to_extension($dudu));
+        if (1 == (preg_match('/\.[a-z,A-Z]{1,4}$/i', // переделать?
+                $_FILES['newFile']['name'], $extensionArr))) {
+            var_dump($extensionArr);
+            $newImgAddress = __DIR__ . '/images/' . uniqid() . $extensionArr[0];
+//            $newImgAddress = __DIR__ . '/images/' . '5c6ddc7f77678' . $extensionArr[0];
+            echo $newImgAddress, ' ';
 
-        /* Преддохранение от перезаписи в случае совпадения имени файла. */
-        $i = 0;
-        while (true == (file_exists($newImgAddress))) { // бесконечность
-            $newImgAddress = __DIR__ . '/images/' . '5c6ddc7f77678' . $extensionArr[0];
-            $i++;
-            echo ' цикл ', $i;
-            assert(100 >= $i);
-            if (100 <= $i) {
-                $signal = 1;
-                break;
+            /* Предохранение от перезаписи в случае маловероятного совпадения имени файла. */
+            $i = 0;
+            while (true == (file_exists($newImgAddress))) {
+                $newImgAddress = __DIR__ . '/images/' . uniqid() . $extensionArr[0];
+                $i++;
+//                echo ' цикл ', $i;
+                assert(100 >= $i);
+                if (100 <= $i) { // Прерывание бесконечного цикла на всякий случай.
+                    $alert = 1;
+                    break;
+                }
             }
-        }
-        if (1 != $signal) {
-            $uploadedFile = move_uploaded_file($_FILES['newFile']['tmp_name'], $newImgAddress);
-            if (file_exists($newImgAddress)) { // если с таким именем уже был файл7
-                echo 'Файл загружен.';
+
+            /* Если новое имя для файла не совпадает с уже существующим, то загружаем файл. */
+            if (!isset($alert)) {
+                $uploadedFile = move_uploaded_file($_FILES['newFile']['tmp_name'], $newImgAddress);
+                /* Проверка на случай ошибки при загрузке. */
+                if (file_exists($newImgAddress)) {
+                    echo 'Файл загружен.';
+                } else {
+                    echo UPLOAD_ERROR;
+                }
+            } else {
+                echo UPLOAD_ERROR;
             }
         } else {
             echo UPLOAD_ERROR;
         }
     } else {
-        echo UPLOAD_ERROR;
+        echo ' НЕ КАРТИНКА '; // исправить
     }
     unset($_FILES['newFile']);
 }
