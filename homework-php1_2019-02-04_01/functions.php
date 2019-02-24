bd367c5a6382e16ef35ec3fee9e71306f5631cad
sculptormuur <amorey@yandex.ru>
2019-02-24 17:39:09 +0000

    задание 5

--- homework-php1_2019-02-04_01/functions.php
+++ homework-php1_2019-02-04_01/functions.php
@@ -3,18 +3,18 @@
 function getUsersList()
 {
     return [
-        'Демьян Варфоломеевич' => 'BD2EA61B385E5034DD46A8B4C320459652DA7438512802C780E4BD0F0902A2C9',
-        'Глаша' => '29674F2387F91CA7CF06EF10F42579E3BA289C62F46DBA6A7EDCDFD27F4E889E',
-        'Афанасий' => '675AADA27E7D9C7FE7EA48C5DED6B89513BFD18644D3552B06930B1D2097D741',
-        '_nightmare666_' => '9D38F055BC3A493B89E58B3907E8752FA9F53ACB750C3429D0770DE7B9B83671',
-        'Пантелеимон' => '488D19428607D30B9CE650C858D0B370308298C7FFC463E80D6F279B78A28308',
-        'Купава' => 'FCAE0D0492FDC7EDB34691626487F5F808EDDDFAEA01B46330EC76B7C1ABC8EC',
-        'ule4ka777' => '8D969EEF6ECAD3C29A3A629280E686CF0C3F5D5A86AFF3CA12020C923ADC6C92',
-        'Кузьма' => 'A795175FD0362310E3FC9B610A26D6A3ACC7904BDC52ED2B9511BE34F7A2A4A8',
-        '-=МяТнаЯ КоНфЕтКа=-' => '65E84BE33532FB784C48129675F9EFF3A682B27168C0EA744B2CF58EE02337C5',
-        'Доброжир Всеволодович' => '80D66C2F3008D64C829EADA5110853D25E84112AFD42E1611484A78C9EE22E36',
-        'John Doe' => '2F183A4E64493AF3F377F745EDA502363CD3E7EF6E4D266D444758DE0A85FCC8',
-        'Иннокентий Шниперсон' => 'D5F69E77CAF0336421C8C86D679B41B8D8F778C9D31FAB2A9C49E1997689FE87'
+        'Демьян Варфоломеевич' => '$2y$10$xO3TRiU2JZ7xPZb2m1yq9ef1cQ.vfWd9Rs84ApWoDUGFUpds87pvO',
+        'Глаша' => '$2y$10$EDSWCYKk08exosl1grlCEuqWTMnnxsFYIc7N1ZBQIYsi4R.Cj.2bm',
+        'Афанасий' => '$2y$10$tYjRDQPW3nptcR1oZxIg.uzdCDp4oNdI3nTo7SUrtXK0SBJhixgBa',
+        '_nightmare666_' => '$2y$10$D3hyVFoYI3hwiWfBYpLDreoX9wE8ykKR1jkbjf8Dd.hOptAc/oqzi',
+        'Пантелеимон' => '$2y$10$Uc25qQdqCrVaTKpxNqA5H.vxYkvGgIGJpTFa56REMNBN3MuAR16DK',
+        'Купава' => '$2y$10$YR9.M1LUCSTpRIb6d603w.jDAED9W3/HsqvXeG1WEDZTFrHIB4w6m',
+        'ule4ka777' => '$2y$10$2Ayp9E1Xkb0cPZJ9Pg.RPOkZFR6wlsC02873JzdDGLyfXbCaKnzzO',
+        'Кузьма' => '$2y$10$LcGV2MXrVmP4dilOx94z6eNdzfO9lW35Fh3c8IeIKnKqnwWJFRarm',
+        '-=МяТнаЯ КоНфЕтКа=-' => '$2y$10$pfosfDbVmRzGEgIu05dNsufkAjW.mx1P0vIbTKj1I6gfoDokCSypi',
+        'Доброжир Всеволодович' => '$2y$10$jDu63PqMkNdw9SNvic5kwOv.MiR0bsqrSXenlI9t9Ypmk7iOCOCSi',
+        'John Doe' => '$2y$10$JDfec9mDicMHFrPPiMG2buRWCv9p.nGEuC3XbOxwnT9obdTmRtDP.',
+        'Иннокентий Шниперсон' => '$2y$10$79rhTYPGdMiMhda7bgvhf.uL0yBRI.J1C0HykhndvSLlt3yDYYljG'
     ];
 }
 
@@ -29,12 +29,19 @@ function checkPassword($login, $password)
         return false;
     } else {
         $usersArray = getUsersList();
-        $savedPasswordHash = strtolower($usersArray[$login]);
-        $enteredPasswordHash = hash('sha256', $password);
-        if ($enteredPasswordHash !== $savedPasswordHash) {
+        $savedPasswordHash = $usersArray[$login];
+        if (true != password_verify($password, $savedPasswordHash)) {
             return false;
         } else {
             return true;
         }
+    }
+}
+
+function getCurrentUser() {
+    if(!isset($_SESSION['username'])) {
+        return null;
+    } else {
+        return $_SESSION['username'];
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -1,4 +1,31 @@
+<html>
+<head>
+    <title> test </title>
+</head>
+<body>
+
+<pre>
 <?php
+
+
+function mb_strcasecmp($str1, $str2, $encoding = null) {
+    if (null === $encoding) { $encoding = mb_internal_encoding(); }
+    return strcmp(mb_strtoupper($str1, $encoding), mb_strtoupper($str2, $encoding));
+}
+/* strcasecmp: мёд > мЁд (1), мед > мЕд (32), мад > мАд (32),
+ * мпд > мПд (32), мgд = мGд (0),  е > Е (32), ё > Ё (1)
+ * */
+$str1 = 'ё';
+$str2 = 'Ё';
+$result = mb_strcasecmp($str1, $str2);
+if ($result < 0) {
+    echo $str1 . ' < ' . $str2, PHP_EOL;
+} elseif ($result > 0) {
+    echo $str1 . ' > ' . $str2, PHP_EOL;
+} else {
+    echo $str1 . ' = ' . $str2, PHP_EOL;
+}
+echo $result;
 
 //var_dump(1);
 //var_dump(2);
@@ -6,9 +33,15 @@
 //var_dump('ё');
 //var_dump('Е');
 //var_dump('Ё');
-var_dump(strcasecmp('е', 'ё'));
-var_dump(strcasecmp('Е', 'Ё'));
-var_dump(mb_ord('е'));
-var_dump(mb_ord('ё'));
-var_dump(mb_ord('Е'));
-var_dump(mb_ord('Ё'));
+//var_dump(strcasecmp('е', 'ё'));
+//var_dump(strcasecmp('Е', 'Ё'));
+//var_dump(mb_ord('е'));
+//var_dump(mb_ord('ё'));
+//var_dump(mb_ord('Е'));
+//var_dump(mb_ord('Ё'));
+
+?>
+</pre>
+
+</body>
+</html>
